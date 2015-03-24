cd170c46572069d82227fa3bc0950b28b2a8f7c0
Aleksandr Dodatko <a.dodatko@healthjoy.com>
2015-03-24 14:14:31 +0200

    [framework] Removed obsolete test files
    The library has no tests

---
+++ JTSHardwareInfo-iOS-dynamic/JTSHardwareInfo-iOS-dynamic.h
@@ -0,0 +1,19 @@
+//
+//  JTSHardwareInfo-iOS-dynamic.h
+//  JTSHardwareInfo-iOS-dynamic
+//
+//  Created by Oleksandr Dodatko on 3/24/15.
+//  Copyright (c) 2015 healthjoy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JTSHardwareInfo-iOS-dynamic.
+FOUNDATION_EXPORT double JTSHardwareInfo_iOS_dynamicVersionNumber;
+
+//! Project version string for JTSHardwareInfo-iOS-dynamic.
+FOUNDATION_EXPORT const unsigned char JTSHardwareInfo_iOS_dynamicVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JTSHardwareInfo_iOS_dynamic/PublicHeader.h>
+
+
---
+++ JTSHardwareInfo-iOS-static/JTSHardwareInfo-iOS-static.h
@@ -0,0 +1,19 @@
+//
+//  JTSHardwareInfo-iOS-static.h
+//  JTSHardwareInfo-iOS-static
+//
+//  Created by Oleksandr Dodatko on 3/24/15.
+//  Copyright (c) 2015 healthjoy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JTSHardwareInfo-iOS-static.
+FOUNDATION_EXPORT double JTSHardwareInfo_iOS_staticVersionNumber;
+
+//! Project version string for JTSHardwareInfo-iOS-static.
+FOUNDATION_EXPORT const unsigned char JTSHardwareInfo_iOS_staticVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JTSHardwareInfo_iOS_static/PublicHeader.h>
+
+
